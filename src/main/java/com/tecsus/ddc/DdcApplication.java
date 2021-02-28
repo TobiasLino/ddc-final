8095870ee173b7c0a126b806584c04ed939834dd
TobiasLino <tobias.lino@sonda.com>
2021-02-28 10:43:26 -0300

    initial commit

---
+++ src/main/java/com/tecsus/ddc/DdcApplication.java
@@ -0,0 +1,13 @@
+package com.tecsus.ddc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DdcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DdcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tecsus/ddc/DdcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tecsus.ddc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DdcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
