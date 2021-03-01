5f4a44da8e1246a6f601dcb62b7b7fe618e304dc
TobiasLino <tobias.lino@sonda.com>
2021-03-01 00:32:30 -0300

    fix: ambiguous mapping

--- src/main/java/com/tecsus/ddc/restapi/BillController.java
+++ src/main/java/com/tecsus/ddc/restapi/BillController.java
@@ -3,15 +3,17 @@ package com.tecsus.ddc.restapi;
 import com.tecsus.ddc.model.bill.BillService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/bills")
+@RestController
+@RequestMapping("/rest/bills")
 public class BillController {
 
     @Autowired
     private BillService billService;
 
-    @GetMapping("/get")
+    @GetMapping("/all")
     public String findALl() {
         return billService.findAll();
     }
